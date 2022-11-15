f3062855601c0d82796a696ee3c7f921e759f6b1
Felipe Lima <felipe.lima0160@gmail.com>
2022-11-15 12:25:37 -0300

    adicionando redirecionamento no botão

--- js/main.js
+++ js/main.js
@@ -3,5 +3,4 @@ const botao = document.querySelector('#botao');
 botao.addEventListener('click', function (event) {
     event.defaultPrevented();
 
-    window.location.href = "../restuldos.html"
 })
