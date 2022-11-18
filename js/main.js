af09ef6ac47e34c2972533dc6e64f235ad7258f3
Felipe Lima <felipe.lima0160@gmail.com>
2022-11-18 11:37:48 -0300

    Dividido JS em arquivos e excluida a função apresnetação e inserida no evento click

--- js/main.js
+++ js/main.js
@@ -4,57 +4,26 @@ botao.addEventListener('click', function (event) {
     event.preventDefault();
 
     const formulario = document.querySelector('#form');
+    const paragrafo  = document.querySelector('#apresentacao');
     const nome       = document.querySelector('#nome').value;
     const tamanho    = document.querySelector('#tamanho').value;
     const cor        = document.querySelector('#cor').value.toLowerCase();
-    const vestido    = vestidos(tamanho, cor)
+    const vestido    = vestidos(tamanho, cor);
     const erros      = validaForm(nome, tamanho, cor, vestido);
-    
+ 
     if (erros.length > 0) {
         exibeMensagensErro(erros)
-        return;
+        paragrafo.textContent = ''
+        return
     }
 
+    if (nome.length > 0) {
+        paragrafo.textContent = `Olá, ${nome}. Aqui está o tão esperado vestido ideal.`
+    }   
+
     exibeMensagensErro(erros);
-    apresentacao(nome);
     console.log(vestido)
 
     formulario.reset();
 })
 
-function apresentacao (nome) {
-    const paragrafo = document.querySelector('#apresentacao');
-    
-    if (nome.length > 0) {
-        paragrafo.textContent = `Olá, ${nome}. Aqui está o tão esperado vestido ideal. `
-    }     
-}
-
-function vestidos (tamanho, cor) {
-    const vestidos = [
-        {
-            id: 1,
-            vestidoLiso: "./assets/img/vestidos/verdePLiso",
-            vestidoBordado: "./assets/img/vestidos/verdePBordado",
-            tamanho: 36,
-            cor: "verde",
-            tipo: "liso"
-        }, {
-            id: 2,
-            vestido: "#",
-            tamanho: 40,
-            cor: "azul",
-            tipo: "bordado"
-        }
-    ]
-
-    const pesquisa  = vestidos.find( vestido => vestido.tamanho == tamanho && vestido.cor == cor)
-    
-    if (typeof pesquisa == 'object') {
-        const resultado = [pesquisa.vestidoLiso, pesquisa.vestidoBordado] 
-        return resultado 
-    } else {
-        const resultado2 = "undefined"
-        return resultado2 
-    }
-}
---
+++ js/vestidos.js
@@ -0,0 +1,69 @@
+function vestidos (tamanho, cor) {
+    const vestidos = [
+        {
+            id: 1,
+            vestidoLiso: "./assets/img/vestidos/verdePLiso",
+            vestidoBordado: "./assets/img/vestidos/verdePBordado",
+            tamanho: 38,
+            cor: "verde",
+        }, {
+            id: 2,
+            vestidoLiso: "./assets/img/vestidos/azulPLiso",
+            vestidoBordado: "./assets/img/vestidos/azulPBordado",
+            tamanho: 38,
+            cor: "azul",
+        }, {
+            id: 3,
+            vestidoLiso: "./assets/img/vestidos/vermelhoPLiso",
+            vestidoBordado: "./assets/img/vestidos/vermelhoPBordado",
+            tamanho: 38,
+            cor: "vermelho",
+        }, {
+            id: 4,
+            vestidoLiso: "./assets/img/vestidos/verdeMLiso",
+            vestidoBordado: "./assets/img/vestidos/verdeMBordado",
+            tamanho: 42,
+            cor: "verde",
+        }, {
+            id: 5,
+            vestidoLiso: "./assets/img/vestidos/azulMLiso",
+            vestidoBordado: "./assets/img/vestidos/azulMBordado",
+            tamanho: 42,
+            cor: "azul",
+        }, {
+            id: 6,
+            vestidoLiso: "./assets/img/vestidos/vermelhoMLiso",
+            vestidoBordado: "./assets/img/vestidos/vermelhoMBordado",
+            tamanho: 42,
+            cor: "vermelho",
+        }, {
+            id: 7,
+            vestidoLiso: "./assets/img/vestidos/verdeGLiso",
+            vestidoBordado: "./assets/img/vestidos/verdeGBordado",
+            tamanho: 46,
+            cor: "verde",
+        }, {
+            id: 8,
+            vestidoLiso: "./assets/img/vestidos/azulGLiso",
+            vestidoBordado: "./assets/img/vestidos/azulGBordado",
+            tamanho: 46,
+            cor: "azul",
+        }, {
+            id: 9,
+            vestidoLiso: "./assets/img/vestidos/vermelhoGLiso",
+            vestidoBordado: "./assets/img/vestidos/vermelhoGBordado",
+            tamanho: 46,
+            cor: "vermelho",
+        } 
+    ]
+
+    const pesquisa  = vestidos.find( vestido => vestido.tamanho == tamanho && vestido.cor == cor)
+    
+    if (typeof pesquisa == 'object') {
+        const resultado = [pesquisa.vestidoLiso, pesquisa.vestidoBordado] 
+        return resultado 
+    } else {
+        const resultado2 = "undefined"
+        return resultado2 
+    }
+}
